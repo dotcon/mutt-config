cc9cfe5d1ab0b224727644c03e6e26c2459af372
Herbert Shen <ishbguy@hotmail.com>
2018-07-18 01:54:45 +0800

    add mkdirs.sh to avoid dirs not exist error

---
+++ bin/mkdirs.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
+
+# source guard
+[[ $MKDIRS_SOURCED -eq 1 ]] && return
+declare -r MKDIRS_SOURCED=1
+declare -r MKDIRS_ABS_SRC="$(realpath "${BASH_SOURCE[0]}")"
+declare -r MKDIRS_ABS_DIR="$(dirname "$MKDIRS_ABS_SRC")"
+
+mkdirs() {
+    local cache="$1"; shift
+    [[ -n $cache ]] || return 1
+    for dir in "$@"; do
+        [[ -d $cache/$dir ]] || mkdir -p $cache/$dir
+    done
+}
+
+[[ ${FUNCNAME[0]} == "main" ]] \
+    && mkdirs "$@"
+
+# vim:set ft=sh ts=4 sw=4:
