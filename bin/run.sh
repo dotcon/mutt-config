da9ff02ba761dfe0b922cc8aa0ed3ef4943383dd
Herbert Shen <ishbguy@hotmail.com>
2018-07-17 20:41:44 +0800

    run cmd with run.sh to quiet command not fond error

---
+++ bin/run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Copyright (c) 2018 Herbert Shen <ishbguy@hotmail.com> All Rights Reserved.
+# Released under the terms of the MIT License.
+
+# source guard
+[[ $RUN_SOURCED -eq 1 ]] && return
+declare -r RUN_SOURCED=1
+declare -r RUN_ABS_SRC="$(realpath "${BASH_SOURCE[0]}")"
+declare -r RUN_ABS_DIR="$(dirname "$RUN_ABS_SRC")"
+
+run() {
+    local PRONAME=run
+    local VERSION="v0.0.1"
+    local HELP=$(cat <<EOF
+$PRONAME $VERSION
+$PRONAME subcmd [args...]
+
+This program is released under the terms of MIT License.
+EOF
+)
+    local cmd="$1"; shift
+    [[ -n $cmd ]] && hash $cmd &>/dev/null || return 1
+
+    "$cmd" "$@"
+}
+
+[[ ${FUNCNAME[0]} == "main" ]] \
+    && run "$@"
+
+# vim:set ft=sh ts=4 sw=4:
